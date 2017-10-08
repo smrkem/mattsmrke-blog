ce2d649aeab2da8b827473415e4f1789b67a7110
matt <smrkem@gmail.com>
2017-10-08 15:36:35 -0400

    adds post assets and initial fetching

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,12 +1,52 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
 
-const Posts = (props) => {
-  console.log("Props:", props)
-  return(
-    <div className="posts">
-      <h1>My Posts</h1>
-    </div>
-  )
+class Posts extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/assets/posts/posts.json')
+      .then(res => {
+        this.checkResponse(res)
+        return res.json()
+      })
+      .then(posts => {
+        this.setState({posts})
+      })
+  }
+
+  render() {
+    console.log("Props:", this.props)
+    console.log("State:", this.state)
+    return(
+      <div className="posts">
+        <h1>My Posts</h1>
+        <div>{
+          this.state.posts.map((post, i) => {
+            return (
+              <div key={i}>
+                <NavLink to={`/posts/${post.slug}`} >{post.title}</NavLink>
+              </div>
+            )
+          })
+        }</div>
+      </div>
+    )
+  }
+
+  checkResponse(response) {
+    if (!response.ok) {
+      console.log("CHECKING RESPONSE", response)
+      console.log("Error...")
+      throw Error("Api fetch failed :(")
+    }
+  }
 }
 
 export default Posts
